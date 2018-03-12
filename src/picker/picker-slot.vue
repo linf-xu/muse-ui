cf22206c96283151e71bb2d788a21b2ba379e0af
linfxu <18903811286@163.com>
2018-03-12 17:20:36 +0800

    2

--- src/picker/picker-slot.vue
+++ src/picker/picker-slot.vue
@@ -13,7 +13,9 @@ import Vue from 'vue'
 import Drag from '../utils/drag'
 import translateUtil from '../utils/translate'
 import * as domUtil from '../utils/domUtil'
-const ITEM_HEIGHT = Math.round(document.body.clientWidth/100*12)
+let bodyW = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
+const ITEM_HEIGHT = Math.round(bodyW / 100 * 12)
+console.log(ITEM_HEIGHT)
 export default {
   props: {
     divider: {
